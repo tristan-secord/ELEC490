ea2dfec0135aad174ba53e254c7baf36828e4642
tristan-secord <tristan.secord@hotmail.com>
2015-01-19 19:00:33 -0500

    added start button and new activity

--- FitnessTracker/app/src/main/java/com/example/elec490/fitnesstracker/FinishFragment.java
+++ FitnessTracker/app/src/main/java/com/example/elec490/fitnesstracker/FinishFragment.java
@@ -1,7 +1,8 @@
 package com.example.elec490.fitnesstracker;
 
-import com.example.elec490.fitnesstracker.R;
+import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -9,20 +10,21 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-
-public class StartFragment extends Fragment{
+public class FinishFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_start, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_finish, container, false);
 
-        final Button startButton = (Button) rootView.findViewById(R.id.start);
-        startButton.setOnClickListener(new View.OnClickListener() {
+        final Button finishButton = (Button) rootView.findViewById(R.id.finish);
+        finishButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent StartWorkout = new Intent(getActivity(), monitorVitalSigns.class);
                 startActivity(StartWorkout);
             }
         });
 
+
         return rootView;
     }
+
 }
---
+++ FitnessTracker/app/src/main/java/com/example/elec490/fitnesstracker/VitalSignsFragment.java
@@ -0,0 +1,20 @@
+package com.example.elec490.fitnesstracker;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class VitalSignsFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_vital_signs, container, false);
+
+        return rootView;
+    }
+
+
+}
